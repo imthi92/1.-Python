1e81279bfbc67b317cda5ba4f89ab7a6f4ca7880
Mohammed Imthiyaz <mohammedimthiyaz832@gmail.com>
2023-04-05 15:44:10 +0530

    Add files via upload

---
+++ 8.Babystep-Control structure.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+print("Welcome to Assignment-1")
+
+
+# In[3]:
+
+
+Num1= 10
+Num2= 30
+Add=Num1+Num2
+print(Add)
+
+
+# In[8]:
+
+
+#Enter the BMI Index:34
+ #   Very Overweight
+    
+BMI=int(input("Enter the BMI Index:"))
+   
+
+
+# In[9]:
+
+
+if(BMI<18.5):
+       print("Underweight")
+elif(BMI<25):
+   print("Normalweight")
+elif(BMI<29):
+   print("Overweight")
+elif(BMI<35):
+   print("Obesity class 1")    
+elif(BMI<39):
+   print("Obesity class 2")        
+else:
+   print("Obesity class 3")        
+
+
+# In[ ]:
+
+
+
+
