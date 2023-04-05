677a7da72bb7296a26c6bc99a68733cb5efb601a
Mohammed Imthiyaz <mohammedimthiyaz832@gmail.com>
2023-04-05 16:07:53 +0530

    Add files via upload

---
+++ 1.python assignment continuation.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+a=43
+b=34
+add=a+b
+print(add)
+
+
+# In[4]:
+
+
+a=43
+b=34
+Sub=a-b
+print(Sub)
+
+
+# In[6]:
+
+
+a= 63
+b= 97
+Mul =a*b
+print(Mul)
+
+
+# In[9]:
+
+
+a= 76
+b= 31
+FloatDiv = a/b
+print(FloatDiv)
+
+
+# In[11]:
+
+
+a= 76
+b= 31
+FloorDiv = a//b
+print(FloorDiv)
+
+
+# In[12]:
+
+
+a= 76
+b= 31
+Modulo = a%b
+print(Modulo)
+
+
+# In[15]:
+
+
+a=4
+b=4
+Power = a**b
+print(Power)
+
+
+# In[ ]:
+
+
+
+
