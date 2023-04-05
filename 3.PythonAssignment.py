a6931f8512c49f16b2e3d6b8829046e474d2d935
Mohammed Imthiyaz <mohammedimthiyaz832@gmail.com>
2023-04-05 19:56:00 +0530

    Add files via upload

---
+++ 3.PythonAssignment.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+i=int(input("Enter vale for i:"))
+if(i==10):
+        print("CORRECT")
+
+
+# In[ ]:
+
+
+# Check the password, using if and else. Enter the password: HOPE@123 Your password is correct
+
+
+# In[6]:
+
+
+password=input("Enter the password:")
+if(password=="HOPE@123"):
+    print("Your password is correct")
+else:
+    print("Your password is incorrect")
+    
+
+
+# In[ ]:
+
+
+# Find whether given number is positive or negative. Enter any number:1 No is positive
+
+
+# In[9]:
+
+
+value=int(input("Enter any number:"))
+if(value<0):
+    print("THE GIVEN NUMBER IS NEGATIVE",value)
+else:
+        print("THE GIVEN NUMBER IS POSITIVE",value)
+
+
+# In[13]:
+
+
+# Check whether the given number is divisible by 5. Enter a number to check:22. No is not divisible by 5
+value=int(input("Enter the value to check whether divisible by 5 or not"))
+if(value%5==0):
+    print("Yes,Divisible by 5")
+else:
+    print("No,not divisible by 5")
+
+
+# In[ ]:
+
+
+
+
