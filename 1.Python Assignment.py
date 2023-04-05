c94d5d85bfd2deb1ab0df3f789d943457ba69792
Mohammed Imthiyaz <mohammedimthiyaz832@gmail.com>
2023-04-05 16:07:32 +0530

    Add files via upload

---
+++ 1.Python Assignment.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("HOPE AI")
+
+
+# In[3]:
+
+
+print=input("Enter your institute name? :")
+
+
+# In[5]:
+
+
+print=input("Enter your name:")
+print=int(input("Enter your age:"))
+print=input("Enter your school name:")
+print=input("Enter your drgree:")
+
