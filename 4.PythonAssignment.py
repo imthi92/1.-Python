500b5a9656ac8d77e11c545f591b152c686fe266
Mohammed Imthiyaz <mohammedimthiyaz832@gmail.com>
2023-04-06 17:09:34 +0530

    Add files via upload

---
+++ 4.PythonAssignment.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# Create Function. Subfields(). Sub-fields in AI are:Machine LearningNeural NetworksVisionRoboticsSpeech ProcessingNatural Language Processing
+
+
+# In[33]:
+
+
+def Subfields():
+   #value=input("How may I help you?:")
+    menu="Sub-fields in AI are:Machine Learning Neural NetworksVision Robotics Speech Processing Natural Language Processing"
+
+
+# In[35]:
+
+
+subs=Subfields()
+print (menu)
+
+
+# In[ ]:
+
+
+# Create Function. OddEven(). Enter a number: 44 is Even number
+
+
+# In[41]:
+
+
+def OddEven():
+    value=int(input("Enter a number"))
+    if (value%2==0):
+        print(value,"is a Even number")
+        variabledeclaration="The given number is a Even number"
+    else:
+        print(value,"is an Odd number")
+        variabledeclaration="The given number is an Odd number"
+        return variabledeclaration
+
+
+# In[43]:
+
+
+OddEven()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
